c65fdf69dddeb23c7513b9fca866e8b266483cec
Liliana Olczak <Lili@Prometheus.local>
2019-04-16 15:55:42 +0200

    implemented first test-method ->  assertTrue checks if Stack is empty

--- Stack/src/test/java/at/fhj/msd/StringStackTest.java
+++ Stack/src/test/java/at/fhj/msd/StringStackTest.java
@@ -1,57 +1,56 @@
 package at.fhj.msd;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
-public class StringStackTest
-{
+public class StringStackTest {
     public Stack s;
-    
+
     @Before
-    public void setup() throws Exception
-    {
+    public void setup() throws Exception {
         s = new StringStack(5); // keep size of 5 !!
     }
-    
+
     /**
      * tests if stack is empty after initialization
-     * 
+     * <p>
      * use assert
      * - assertTrue to check it's empty
      */
     @Test
-    public void testIsEmpty() throws Exception
-    {
+    public void testIsEmpty() throws Exception {
+        assertTrue(s.isEmpty());
     }
-    
+
     /**
      * tests if stack is empty after pushing
-     * 
+     * <p>
      * use assert
      * - assertFalse to check it's not empty
      */
     @Test
-    public void testIsNotEmpty() throws Exception
-    {
+    public void testIsNotEmpty() throws Exception {
     }
 
     /**
      * tests push and pop function
-     * 
+     * <p>
      * do following test
      * - use push, 1 or x times
      * - use pull, as often as you use push
      * - use isEmpty to check that's empty again
      * - assert every content of the stack in the correct ordering
-     * @throws Exception 
+     *
+     * @throws Exception
      */
     @Test
-    public void testPushPop() throws Exception
-    {
+    public void testPushPop() throws Exception {
     }
-    
+
     /**
      * Feel Free to insert other test cases for the String Stack Implementation !!!
      */
-    
+
 }
